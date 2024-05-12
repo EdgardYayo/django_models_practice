a79df5e8cd36520d94176a62e4569f44a7cf433f
EdgardYayo <allan.pazos@uabc.edu.mx>
2024-05-12 16:32:36 -0700

    Learning how relationsships works in Django and also how the admin section works

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Book, Author
 # Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
+
+class BookAdmin(admin.ModelAdmin):
+    #readonly_fields = ("slug",)
+    prepopulated_fields = {
+        "slug": ("title",)
+    }
+    list_filter = ("rating", "is_bestselling")
+    list_display = ("title", "author", "rating")
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
